727936af18c925134997e6357072c87fcb5ee39c
Alex Bruckner <alex.bruckner@space-time-solutions.com>
2012-05-24 16:33:45 +0100

    enhanced test method to actually run when code is implemented.

--- test/clear4j/Clear4jTest.java
+++ test/clear4j/Clear4jTest.java
@@ -1,7 +1,9 @@
 package clear4j;
 
+import junit.framework.Assert;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import sun.awt.windows.ThemeReader;
 
 /**
  * User: alexb
@@ -10,16 +12,35 @@ import sun.awt.windows.ThemeReader;
  */
 public class Clear4jTest {
 
-    @Test
-    public void testScenario(){
+    @BeforeClass
+    public static void setup(){
+        FileUtils.writeTextToFile(TestConfig.TEST_FILE_PATH.getValue(), TestConfig.TEST_FILE_CONTENT.getValue());
+    }
 
-        // under the bonnet: we send an instruction message "load" to the file processor with payload "file"
-        // instruct(Enum 'The' -> seclection: FileProcessor) returns a processor.FileProcessor
+    @AfterClass
+    public static void tearDown(){
+        FileUtils.removeFile(TestConfig.TEST_FILE_PATH.getValue());
+    }
+
+    @Test
+    public void testScenario() {
+
+        // under the bonnet: we send an instruction message "load" to the file processor
+        // with payload Payload.FILE_PATH = "/tmp/test.txt"
+        // instruct(Enum 'The' -> selection: FileProcessor) returns a processor.FileProcessor
         // with certain methods that create instruction messages to be put on the file processor queue.
         // this queue can be local to the jvm or distributed.
-        Clear.instruct(The.FileProcessor).toLoad(aFile);
+        Workflow workflow = Clear.instruct(The.FileProcessor).toLoad(TestConfig.TEST_FILE_PATH.getValue());
+        Payload payload = workflow.waitFor();
+        String text1 = payload.get(Payload.TEXT);
 
+        // load it the boring way
+        String text2 = FileUtils.loadTextFromFile(TestConfig.TEST_FILE_PATH.getValue());
+
+        Assert.assertEquals(text2, text1);
 
     }
+
+
 
 }
---
+++ test/clear4j/TestConfig.java
@@ -0,0 +1,20 @@
+package clear4j;
+
+/**
+ * User: alexb
+ * Date: 24/05/12
+ * Time: 16:18
+ */
+public enum TestConfig {
+    TEST_FILE_PATH("/tmp/test.txt"),
+    TEST_FILE_CONTENT("This is\na sample\nfile content.");
+
+    private String value;
+    private TestConfig(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
