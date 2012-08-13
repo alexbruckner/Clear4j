afa3ae70a46545d0aba5c5de999bba58faca418e
alex <alex.bruckner@space-time-solutions.com>
2012-08-13 20:28:15 +0100

    initial check in to allow for @Param args in processors

--- src/main/java/clear4j/processors/PrintProcessor.java
+++ src/main/java/clear4j/processors/PrintProcessor.java
@@ -19,11 +19,16 @@ public class PrintProcessor {
         return value;
 	}
 	
+//	@Function //TODO
+//	public Object println(Object value, @Param("key1") String param1, @Param("key2") String param2){
+//		System.out.println(String.format("Extra Args: Key1: [%s], Key2: [%s]", param1, param2));
+//		return println(value);
+//	}
+
 	//extra parameters
 	@Function
-	public Object println(Object value, Param<?>... params){
-		System.out.println(String.format("Extra Args: [%s]", Arrays.toString(params)));
+	public Object println(Object value, Param... args){
+		System.out.println(String.format("Extra Args: [%s]", Arrays.toString(args)));
 		return println(value);
 	}
-	
 }
